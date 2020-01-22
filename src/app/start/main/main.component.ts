3e52dea46482c6bf3c239f6521acad53cf92613a
morrisjdev <morrisj@live.de>
2020-01-22 09:33:09 +0100

    updated documentation main page

--- src/app/start/main/main.component.ts
+++ src/app/start/main/main.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.less']
+  styleUrls: ['./main.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainComponent implements OnInit {
 
