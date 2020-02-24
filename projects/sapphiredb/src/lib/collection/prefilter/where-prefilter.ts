ed5ebecbdc01aaa1d4c78ba8b5d74f68a8fbc2c0
morrisjdev <morrisj@live.de>
2020-02-24 15:53:13 +0100

    added local execution of where prefilter

--- projects/sapphiredb/src/lib/collection/prefilter/where-prefilter.ts
+++ projects/sapphiredb/src/lib/collection/prefilter/where-prefilter.ts
@@ -1,16 +1,20 @@
 import {IPrefilter} from './iprefilter';
 import {ConditionType} from '../../helper/condition-types';
+import {FilterFunctions} from '../../helper/filter-functions';
 
 export class WherePrefilter<T> implements IPrefilter<T, T[]> {
   prefilterType = 'WherePrefilter';
   conditions: ConditionType<T>;
 
+  private conditionFunction: (value: T) => boolean;
+
   constructor(conditions: ConditionType<T>) {
     this.conditions = conditions;
+    this.conditionFunction = FilterFunctions.convertConditionParts(this.conditions);
   }
 
   public execute(values: T[]) {
-    return values;
+    return values.filter(this.conditionFunction);
   }
 
   public hash() {
