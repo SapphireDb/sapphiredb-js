5f8224b705ca871e41c621e467105d14ffce0344
morrisjdev <morrisj@live.de>
2020-02-26 17:59:14 +0100

    added groups to code examples

--- src/app/shared/doc/doc.component.ts
+++ src/app/shared/doc/doc.component.ts
@@ -1,4 +1,5 @@
 import {Component, ComponentRef, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {SapphireOfflineEntity} from 'sapphiredb';
 
 @Component({
   selector: 'app-doc',
@@ -8,14 +9,15 @@ import {Component, ComponentRef, ElementRef, Input, OnChanges, OnInit, SimpleCha
 export class DocComponent implements OnInit, OnChanges {
   @Input() showContent = true;
 
-  languages: { lang: string, content: string, name: string, nameNormalized: string }[];
+  id = new SapphireOfflineEntity().id;
+  contents: { lang: string, content: string, name: string, section?: string, nameNormalized: string }[][];
   @ViewChild('langElement', {static: true}) langElementRef: ElementRef;
 
   constructor() { }
 
   compileTemplate() {
     if (this.langElementRef.nativeElement.innerText) {
-      this.languages = this.langElementRef.nativeElement.innerText
+      this.contents = this.langElementRef.nativeElement.innerText
         .split('\\f:(')
         .filter(v => !!v.trim())
         .map(v => {
@@ -24,12 +26,14 @@ export class DocComponent implements OnInit, OnChanges {
           return {
             name: languageParts[1],
             lang: languageParts[0],
-            nameNormalized: languageParts[1].split('.').join(''),
+            section: languageParts[2],
+            nameNormalized: languageParts[1].split('.').join('') + '_id' + new SapphireOfflineEntity().id,
             content: v.substr(v.indexOf(')') + 1).split('\\n').map(l => {
               return l.trim().split('\\t').map(iv => iv.trim()).join('  ');
             }).join('\n')
           };
-        });
+        })
+        .GroupBy(v => v.section);
     }
   }
 
