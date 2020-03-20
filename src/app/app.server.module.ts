83b9001dd89b610aa0285bf0c4584a0001c896b3
morrisjdev <morrisj@live.de>
2020-03-20 20:10:28 +0100

    updated packages

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
@@ -8,9 +7,8 @@ import { AppComponent } from './app.component';
 @NgModule({
   imports: [
     AppModule,
-    ServerModule,
-    ModuleMapLoaderModule
-  ],
+    ServerModule
+],
   providers: [
     // Add universal-only providers here
   ],
