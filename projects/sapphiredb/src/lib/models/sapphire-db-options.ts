9e8e48b913c6bfe1f9c5fdd162c80e1cd0445cef
Morris Janatzek <mjanatzek@brockhaus-ag.de>
2020-04-01 13:53:32 +0200

    added jsdocs to options

--- projects/sapphiredb/src/lib/models/sapphire-db-options.ts
+++ projects/sapphiredb/src/lib/models/sapphire-db-options.ts
@@ -1,8 +1,36 @@
 export interface SapphireDbOptions {
-  connectionType?: 'websocket'|'sse'|'poll';
+  /**
+   * The connection to use. If left empty the best suiting connection type is automatically selected
+   */
+  connectionType?: 'websocket' | 'sse' | 'poll';
+
+  /**
+   * The base url of the server. If left empty the hostname and port of the current application is used.
+   */
   serverBaseUrl?: string;
+
+  /**
+   * Use ssl to connect to server. If left empty the current ssl-settings are used.
+   */
   useSsl?: boolean;
+
+  /**
+   * Api key to be used when connecting.
+   */
   apiKey?: string;
+
+  /**
+   * Secret to be used when connecting.
+   */
   apiSecret?: string;
+
+  /**
+   * Enable offline support and try to store collection data and changes locally when not connected to server.
+   */
   offlineSupport?: boolean;
+
+  /**
+   * Enable optimization of offline changes.
+   */
+  offlineOptimization?: boolean;
 }
