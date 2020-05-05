de68c46c555cbf0adda7819f1db9c8b090ec2dcd
morrisjdev <morrisj@live.de>
2020-05-05 16:46:30 +0200

    updated response type of update range response updated documentation for new update api

--- projects/sapphiredb/src/lib/command/update-range/update-range-response.ts
+++ projects/sapphiredb/src/lib/command/update-range/update-range-response.ts
@@ -1,8 +1,9 @@
 import {ResponseBase} from '../response-base';
 import {ValidatedResponseBase} from '../validated-response-base';
+import {CreateResponse} from '../create-range/create-range-response';
 
 export interface UpdateRangeResponse extends ResponseBase {
-  results: UpdateResponse[];
+  results: (UpdateResponse|CreateResponse)[];
 }
 
 export interface UpdateResponse extends ValidatedResponseBase {
