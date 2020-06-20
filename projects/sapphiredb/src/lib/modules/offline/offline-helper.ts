eb247d15cb8befa85af3d2740002d377e6b4da39
morrisjdev <morrisj@live.de>
2020-06-20 17:44:00 +0200

    added offline helper merging changes of update, create and delete

--- projects/sapphiredb/src/lib/modules/offline/offline-helper.ts
+++ projects/sapphiredb/src/lib/modules/offline/offline-helper.ts
@@ -15,10 +15,10 @@ export class OfflineHelper {
     });
   }
 
-  static handleAddCommand(command: DeleteRangeCommand|UpdateRangeCommand|CreateRangeCommand, primaryKeys: string[],
+  static handleAddCommand(command: DeleteRangeCommand | UpdateRangeCommand | CreateRangeCommand, primaryKeys: string[],
                           collectionChanges: CollectionCommandBase[]): boolean {
     if (command.commandType === 'UpdateRangeCommand') {
-      const updateCommand = <UpdateRangeCommand> command;
+      const updateCommand = <UpdateRangeCommand>command;
 
       for (const entry of updateCommand.entries) {
         const filterFunction = FilterFunctions.comparePrimaryKeysFunction(primaryKeys, entry.value);
@@ -34,6 +34,23 @@ export class OfflineHelper {
           }
         }
       }
+    } else if (command.commandType === 'DeleteRangeCommand') {
+      const deleteCommand = <DeleteRangeCommand>command;
+
+      for (const value of deleteCommand.values) {
+        const filterFunction = FilterFunctions.comparePrimaryKeysFunction(primaryKeys, value);
+        const commandIndexWithSameValue = this.getCommandIndexWithSameValue(filterFunction, collectionChanges);
+
+        if (commandIndexWithSameValue !== -1) {
+          if (this.handleAddDeleteCommand(value, collectionChanges, commandIndexWithSameValue, filterFunction)) {
+            deleteCommand.values = deleteCommand.values.filter(v => !filterFunction(v));
+
+            if (deleteCommand.values.length === 0) {
+              return false;
+            }
+          }
+        }
+      }
     }
 
     return true;
@@ -41,15 +58,50 @@ export class OfflineHelper {
 
   static handleAddUpdateCommand(entry: UpdateEntry, collectionChanges: CollectionCommandBase[], commandIndex: number,
                                 filterFunction: (any) => boolean): boolean {
-    const commandWithSameValue = <CreateRangeCommand|UpdateRangeCommand|DeleteRangeCommand>collectionChanges[commandIndex];
+    const commandWithSameValue = <CreateRangeCommand | UpdateRangeCommand | DeleteRangeCommand>collectionChanges[commandIndex];
 
     if (commandWithSameValue.commandType === 'UpdateRangeCommand') {
       const updateRangeCommand = <UpdateRangeCommand>commandWithSameValue;
       const valueIndex = updateRangeCommand.entries.findIndex(v => filterFunction(v.value));
       const oldUpdateEntry = updateRangeCommand.entries[valueIndex];
 
-      // Merge changes of both update entries
-      updateRangeCommand.entries[valueIndex] = new UpdateEntry(oldUpdateEntry.value, { ...oldUpdateEntry.updatedProperties, ...entry.updatedProperties });
+      updateRangeCommand.entries[valueIndex] = new UpdateEntry(oldUpdateEntry.value, {
+        ...oldUpdateEntry.updatedProperties,
+        ...entry.updatedProperties
+      });
+      return true;
+    } else if (commandWithSameValue.commandType === 'CreateRangeCommand') {
+      const createRangeCommand = <CreateRangeCommand>commandWithSameValue;
+      const valueIndex = createRangeCommand.values.findIndex(v => filterFunction(v));
+      createRangeCommand.values[valueIndex] = {
+        ...createRangeCommand.values[valueIndex],
+        ...entry.updatedProperties
+      };
+      return true;
+    }
+
+    return false;
+  }
+
+  static handleAddDeleteCommand(value: any, collectionChanges: CollectionCommandBase[], commandIndex: number,
+                                filterFunction: (any) => boolean): boolean {
+    const commandWithSameValue = <CreateRangeCommand | UpdateRangeCommand | DeleteRangeCommand>collectionChanges[commandIndex];
+
+    if (commandWithSameValue.commandType === 'UpdateRangeCommand') {
+      const updateRangeCommand = <UpdateRangeCommand>commandWithSameValue;
+
+      updateRangeCommand.entries = updateRangeCommand.entries.filter(v => !filterFunction(v.value));
+      if (updateRangeCommand.entries.length === 0) {
+        collectionChanges.splice(commandIndex, 1);
+      }
+    } else if (commandWithSameValue.commandType === 'CreateRangeCommand') {
+      const createRangeCommand = <CreateRangeCommand>commandWithSameValue;
+
+      createRangeCommand.values = createRangeCommand.values.filter(v => !filterFunction(v));
+      if (createRangeCommand.values.length === 0) {
+        collectionChanges.splice(commandIndex, 1);
+      }
+
       return true;
     }
 
