4bdfb245be0ddbfdb7976a6cafc0e0a06624da37
morrisjdev <morrisj@live.de>
2020-07-31 08:36:21 +0200

    added examples for server side queries

--- src/app/data/server-side-query/server-side-query.component.ts
+++ src/app/data/server-side-query/server-side-query.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {SapphireDbService} from 'ng-sapphiredb';
 import {Observable} from 'rxjs';
+import {QueryCollection, SapphireOfflineEntity} from 'sapphiredb';
+import {DialogService} from 'ng-metro4';
 
 @Component({
   selector: 'app-server-side-query',
@@ -9,13 +11,27 @@ import {Observable} from 'rxjs';
 })
 export class ServerSideQueryComponent implements OnInit {
 
+  collection: QueryCollection<any, any[]>;
   values$: Observable<any>;
+  snapshot$: Observable<any>;
 
-  constructor(private db: SapphireDbService) {
-    this.values$ = this.db.query<any>('demo.entries.only_test').values();
+  constructor(private db: SapphireDbService, private dialogService: DialogService) {
+    this.collection = this.db.query<any>('demo.entries.only_test');
+    this.values$ = this.collection.values();
+    this.snapshot$ = this.collection.snapshot();
+  }
+
+  addValue() {
+    this.dialogService.prompt('Content', 'Please enter a new content').subscribe((v) => {
+      this.collection.add({
+        ...new SapphireOfflineEntity(),
+        content: v,
+      });
+    });
   }
 
   ngOnInit(): void {
+
   }
 
 }
