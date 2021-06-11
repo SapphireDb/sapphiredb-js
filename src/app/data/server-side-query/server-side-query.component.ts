1e76f5a395f30cb3c416fa45541cfb383f3218f3
morrisjdev <morrisj@live.de>
2021-06-11 14:15:06 +0200

    added demo and documentation for default query

--- src/app/data/server-side-query/server-side-query.component.ts
+++ src/app/data/server-side-query/server-side-query.component.ts
@@ -12,13 +12,19 @@ import {DialogService} from 'ng-metro4';
 export class ServerSideQueryComponent implements OnInit {
 
   collection: QueryCollection<any, any[]>;
-  values$: Observable<any>;
-  snapshot$: Observable<any>;
+  values$: Observable<any[]>;
+  snapshot$: Observable<any[]>;
+
+  defaultQueryCollection: QueryCollection<any, any[]>;
+  defaultQueryValues$: Observable<any[]>;
 
   constructor(private db: SapphireDbService, private dialogService: DialogService) {
     this.collection = this.db.query<any>('demo.entries.only_test');
     this.values$ = this.collection.values();
     this.snapshot$ = this.collection.snapshot();
+
+    this.defaultQueryCollection = this.db.query<any>('demo.serverSideQueryWithDefaults.by_content');
+    this.defaultQueryValues$ = this.defaultQueryCollection.values();
   }
 
   addValue() {
@@ -28,6 +34,19 @@ export class ServerSideQueryComponent implements OnInit {
         content: v,
       });
     });
+  }
+
+  addValueDefaultQuery() {
+    this.dialogService.prompt('Content', 'Please enter a new content').subscribe((v) => {
+      this.defaultQueryCollection.add({
+        ...new SapphireOfflineEntity(),
+        content: v,
+      });
+    });
+  }
+
+  deleteDefaultQueryValue(entry: any) {
+    this.defaultQueryCollection.remove(entry);
   }
 
   ngOnInit(): void {
